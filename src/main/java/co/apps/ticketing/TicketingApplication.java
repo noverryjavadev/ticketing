ee558a5c1a3a25f989c3175b44bc8235e81bf071
noverryjs <noverry.ambo@iconpln.co.id>
2026-03-20 23:02:25 +0700

    bus fleet service AuditTable

--- src/main/java/co/apps/ticketing/TicketingApplication.java
+++ src/main/java/co/apps/ticketing/TicketingApplication.java
@@ -2,8 +2,10 @@ package co.apps.ticketing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TicketingApplication {
 
 	public static void main(String[] args) {
--- src/main/java/co/apps/ticketing/entity/AuditTable.java
+++ src/main/java/co/apps/ticketing/entity/AuditTable.java
@@ -1,11 +1,17 @@
 package co.apps.ticketing.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -14,17 +20,22 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class AuditTable {
 
+    @CreatedDate
     @Column(name = "created_date")
     private LocalDateTime createdDate;
 
+    @CreatedBy
     @Column(name = "created_by")
     private String createdBy;
 
+    @LastModifiedDate
     @Column(name = "last_updated_date")
     private LocalDateTime lastUpdatedDate;
 
+    @LastModifiedBy
     @Column(name = "last_updated_by")
     private String lastUpdatedBy;
 }
