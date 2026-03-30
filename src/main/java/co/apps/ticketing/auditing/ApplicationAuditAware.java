3354a02dc00c5698c8b9c93eb7699c124208370c
noverryjs <noverry.ambo@iconpln.co.id>
2026-03-30 14:37:54 +0700

    refactor dikit

--- src/main/java/co/apps/ticketing/auditing/ApplicationAuditAware.java
+++ src/main/java/co/apps/ticketing/auditing/ApplicationAuditAware.java
@@ -7,22 +7,6 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class ApplicationAuditAware implements AuditorAware<String> {
-//    @Override
-//    public Optional<Integer> getCurrentAuditor() {
-//        Authentication authentication =
-//                SecurityContextHolder
-//                        .getContext()
-//                        .getAuthentication();
-//        if (authentication == null ||
-//            !authentication.isAuthenticated() ||
-//                authentication instanceof AnonymousAuthenticationToken
-//        ) {
-//            return Optional.empty();
-//        }
-//
-//        User userPrincipal = (User) authentication.getPrincipal();
-//        return Optional.ofNullable(userPrincipal.getId());
-//    }
 
     @Override
     public Optional<String> getCurrentAuditor() {
